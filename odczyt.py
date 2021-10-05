c15e476a638fae457af5b6773bf78b755d7d60e9
Ukasz-Witkowski <lukasz.witkowski99@gmail.com>
2021-10-05 12:44:21 +0200

    Create odczyt.py

---
+++ odczyt.py
@@ -0,0 +1,45 @@
+import time
+import csv
+import matplotlib
+matplotlib.use("tkAgg")
+import matplotlib.pyplot as plt
+import numpy as np
+import pyvisa
+
+
+class Aparature:
+    multimeter = 'USB0::0x164E::0x0DAD::TW00042933::INSTR'
+    def __init__(self):
+        rm = pyvisa.ResourceManager()
+        self.thermometer = rm.open_resource(self.multimeter)
+
+    def measurement(self):
+        temperature = self.thermometer.query("MEAS:RES?")
+        return temperature
+
+aparature = Aparature()
+
+plot_window = 20
+y_var = np.array(np.zeros([plot_window]))
+
+plt.ion()
+fig, ax = plt.subplots()
+line, = ax.plot(y_var)
+
+while True:
+    data = float(aparature.measurement())
+    print(data)
+    with open("test_data.csv","a") as f:
+        writer = csv.writer(f, delimiter=",")
+        writer.writerow([time.time(), data])
+    y_var = np.append(y_var, data )
+    y_var = y_var[1 : plot_window + 1]
+    line.set_ydata(y_var)
+    ax.relim()
+    ax.autoscale_view()
+    fig.canvas.draw()
+    fig.canvas.flush_events()
+    time.sleep(0.1)
+
+def foo():
+    print("abc")
